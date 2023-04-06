3227626678d3f6c17d69e7601fbcd8786f7ba30b
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-06 07:26:45 -0700

    script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,28 +1,18 @@
 #!/usr/bin/env bash
 # Install Nginx if not already installed
-if ! command -v nginx &> /dev/null; then
-    apt-get update -y
-    apt-get install -y nginx
-fi
-
-# Create necessary folders if they don't already exist
+apt update -y
+apt install -y nginx
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
-chown -R ubuntu:ubuntu /data/
-
-# Create fake HTML file for testing
 echo "<!DOCTYPE html>
 <html>
   <head>
   </head>
   <body>
-    <p>Nginx server test</p>
+    Holberton School
   </body>
 </html>" | tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Configure Nginx to serve content
-sed -i '/server_name _;/a location /hbnb_static {\n\talias /data/web_static/current/;\n}\n' /etc/nginx/sites-enabled/default
-service nginx restart
+chown -R ubuntu:ubuntu /data
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo service nginx restart
