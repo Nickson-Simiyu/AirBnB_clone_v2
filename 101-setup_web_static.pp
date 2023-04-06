6cc76a4d04836a13fc42563644b4474aa100f172
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-06 08:43:11 -0700

    updated puppet script

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,78 +1,63 @@
 # Setup the web servers for the deployment of web_static
-class nginx_web_static {
-  package { 'nginx':
-    ensure => 'installed',
-  }
-
-  file { '/data/web_static/releases/test/':
-    ensure  => 'directory',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0755',
-    recurse => true,
-  }
-
-  file { '/data/web_static/shared/':
-    ensure  => 'directory',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0755',
-    recurse => true,
-  }
-
-  file { '/data/web_static/releases/test/index.html':
-    ensure  => 'file',
-    content => '<!DOCTYPE html>
+xec { '/usr/bin/env apt -y update' : }
+-> package { 'nginx':
+  ensure => installed,
+}
+-> file { '/data':
+  ensure  => 'directory'
+}
+-> file { '/data/web_static':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/releases':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/releases/test':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/shared':
+  ensure => 'directory'
+}
+-> file { '/data/web_static/releases/test/index.html':
+  ensure  => 'present',
+  content => "<!DOCTYPE html>
 <html>
   <head>
   </head>
   <body>
-    Holberton School
+    <p>Nginx server test</p>
   </body>
-</html>',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-    mode    => '0644',
-  }
-
-  file { '/data/web_static/current':
-    ensure  => 'link',
-    target  => '/data/web_static/releases/test/',
-    owner   => 'ubuntu',
-    group   => 'ubuntu',
-  }
-
-  file { '/etc/nginx/sites-enabled/default':
-    ensure  => 'file',
-    owner   => 'root',
-    group   => 'root',
-    content => "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-
-    index index.html index.htm index.nginx-debian.html;
-
-    server_name _;
-
-    location / {
-        try_files $uri $uri/ =404;
-    }
-
-    location /hbnb_static/ {
-        alias /data/web_static/current/;
-    }
+</html>"
 }
-",
-  }
-
-  service { 'nginx':
-    ensure => 'running',
-    enable => true,
-    require => Package['nginx'],
-    subscribe => File['/etc/nginx/sites-enabled/default'],
-  }
+-> file { '/data/web_static/current':
+  ensure => 'link',
+  target => '/data/web_static/releases/test'
 }
-
-class { 'nginx_web_static': }
+-> exec { 'chown -R ubuntu:ubuntu /data/':
+  path => '/usr/bin/:/usr/local/bin/:/bin/'
+}
+-> file { '/var/www':
+  ensure => 'directory'
+}
+-> file { '/var/www/html':
+  ensure => 'directory'
+}
+-> file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>"
+}
+exec { 'nginx_conf':
+  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
+  command     => 'sed -i "39i $data" /etc/nginx/sites-enabled/default',
+  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
+}
+-> service { 'nginx':
+  ensure => running,
+}
