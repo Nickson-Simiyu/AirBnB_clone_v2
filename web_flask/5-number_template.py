fb1526c4ff8f333d34df1b352d9025966efa5feb
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-22 12:34:18 -0700

    script that adds fifth view func that displays HTML page if n is int

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" 5. Add fifth view func that displays HTML page if n is int """
+
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    text = text.replace('_', ' ') if text else 'is cool'
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    if isinstance(n, int):
+        return render_template('number.html', number=n)
+    else:
+        return 'Not a valid number', 404
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
