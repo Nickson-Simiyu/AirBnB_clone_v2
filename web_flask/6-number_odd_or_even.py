042d27aaf909f5e337d49feeea62ce5a98453bd2
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-22 13:15:06 -0700

    script that adds fifth view func that displays HTML page if n is int

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+""" 5. Add fifth view func that displays HTML page if n is int """
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    """ Returns 'Hello HBNB!' """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """ Returns 'HBNB' """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """ Returns 'C ' followed by the value of text with underscores replaced by spaces """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_text(text):
+    """ Returns 'Python ' followed by the value of text with underscores replaced by spaces """
+    text = text.replace('_', ' ') if text else 'is cool'
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def number(n):
+    """ Returns 'n is a number' only if n is an integer """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    """ Returns an HTML page with 'Number: n' inside the body tag, only if n is an integer """
+    return render_template('number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>')
+def number_odd_or_even(n):
+    """ Returns an HTML page with 'Number: n is even|odd' inside the body tag, only if n is an integer """
+    if n % 2 == 0:
+        result = '{} is even'.format(n)
+    else:
+        result = '{} is odd'.format(n)
+    return render_template('6-number_odd_or_even.html', n=n, result=result)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
