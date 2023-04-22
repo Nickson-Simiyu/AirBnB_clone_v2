d4cb0a37a32c7446b46a74a79e96eed4819bd35f
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-22 13:22:49 -0700

    script that adds fifth view func that displays HTML page if n is int

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ 5. Add fifth view func that displays HTML page if n is int """
 
-from flask import Flask, request
-from flask import render_template
+from flask import Flask, request, render_template
 
 
 app = Flask(__name__)
@@ -42,8 +41,7 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>')
-def html_num(n):
-    """ display html if n is int. """
+def number_template(n):
     return render_template('5-number.html', n=n)
 
 
