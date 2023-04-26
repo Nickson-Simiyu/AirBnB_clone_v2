4e044921a5844783c765e817c7134a33f3b15cef
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-26 07:13:35 -0700

    script that starts a Flask web application

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-""" 7. Start flask service that does something. """
 
-from flask import Flask, render_template
+"""This module contains the app instance for the AirBnB Web"""
+
 from models import storage
 from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Close the SQLAlchemy session after each request."""
-    storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Display a list of all State objects in alphabetical order."""
-    states = storage.all(State).values()
-    states_sorted = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states_sorted)
+    """enumerate all states"""
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def clean_up(exception=None):
+    """eliminates current Session"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
