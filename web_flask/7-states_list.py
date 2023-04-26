1eff30ad1c44c90658cb3429266732dce86336c8
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-04-26 07:26:16 -0700

    starts flask app

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""This module contains the app instance for the AirBnB Web"""
-
-from models import storage
-from models.state import State
+"""simple flask app
+"""
 from flask import Flask, render_template
-
+from models import storage
 app = Flask(__name__)
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """enumerate all states"""
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def clean_up(exception=None):
-    """eliminates current Session"""
+def shutdown_session(exception=None):
+    """reload storage after each request
+    """
     storage.close()
 
 
-if __name__ == '__main__':
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """list states sorted by name
+    """
+    states = list(storage.all("State").values())
+    states.sort(key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
